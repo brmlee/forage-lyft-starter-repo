992980347b3da4942d27f319e1768af0da8d9c43
Brian Lee <brmlee@umich.edu>
2023-07-05 13:43:00 -0400

    updates spindler and initiailize battery

--- battery/nubbin_battery.py
+++ battery/nubbin_battery.py
@@ -3,7 +3,6 @@ from abc import ABC
 from car import Car
 class NubbinBattery(Car, ABC):
     def __init__(self, last_service_date, current_date):
-        super().__init__(last_service_date)
         self.current_date = current_date
         self.last_service_date = last_service_date
 
--- battery/spindler_battery.py
+++ battery/spindler_battery.py
@@ -2,10 +2,10 @@ from abc import ABC
 
 from car import Car
 class SpindlerBattery(Car, ABC):
-    def __init__(self, last_service_date, current_mileage, last_service_mileage):
-        super().__init__(last_service_date)
-        self.current_mileage = current_mileage
-        self.last_service_mileage = last_service_mileage
+    def __init__(self, last_service_date, current_date):
+        self.current_date = current_date
+        self.last_service_date = last_service_date
+
 
     def battery_should_be_serviced(self):
         return self.curret_date - self.last_service_date > 2
